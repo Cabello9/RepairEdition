00d56e712ea67599a5a4f4d2048b5b3db73c80b7
Cabello9 <jose.cabello.cubero@gmail.com>
2020-02-01 11:44:22 +0100

    Escena del menú principal y comportamiento inicial del menú

---
+++ Assets/Scripts/MainMenu/ButtonBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public float duration = 0.2f;
+
+    public void Animate()
+    {
+        transform.DOScale(new Vector3(0.5f, 0.5f, 0.5f), duration).SetEase(curve);
+    }
+}
---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Image Fade;
+
+    private void DoFade(float duration, Action OnFinish)
+    {
+        Fade.DOFade(1, duration).OnComplete(() => { OnFinish(); });
+    }
+    
+
+    public void LoadGame()
+    {
+        DoFade(0.5f, () => { SceneManager.LoadScene("Game");});
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
