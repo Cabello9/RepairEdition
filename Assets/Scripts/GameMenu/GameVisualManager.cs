77352ced0fcd769c3320b32db2dfca41c51f8ec7
Cabello9 <jose.cabello.cubero@gmail.com>
2020-02-01 12:48:14 +0100

    Aparición de los turnos del jugador y mejora de los tweens de la ficha

---
+++ Assets/Scripts/GameMenu/GameVisualManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameVisualManager : Singleton<GameVisualManager>
+{
+    public Image Fade;
+    public Transform PlayerTurnText;
+
+    public List<Transform> TurnScreenReferencePoints;
+    public float TurnMoveDuration;
+
+    public Color PlayerOneColor;
+    public Color PlayerTwoColor;
+
+    public bool isPlayerOneTurn = true;
+
+    private void Start()
+    {
+        DoFade(0, () => { PlayerTurn(isPlayerOneTurn); });
+    }
+
+    public void DoFade(float endValue, Action OnFinish)
+    {
+        Fade.DOFade(endValue, 2.5f).OnComplete(() => { OnFinish?.Invoke(); });
+    }
+
+    public void PlayerTurn(bool isPlayerOne)
+    {
+        if (isPlayerOne) 
+            ConfigPlayerOneTurn();
+        else
+            ConfigPlayerTwoTurn();
+
+        StartCoroutine(PlayerTurnMovement());
+
+    }
+
+    IEnumerator PlayerTurnMovement()
+    {
+        PlayerTurnText.position = TurnScreenReferencePoints[0].position;
+        PlayerTurnText.gameObject.SetActive(true);    
+        PlayerTurnText.DOMove(TurnScreenReferencePoints[1].position, TurnMoveDuration);
+        yield return new WaitForSeconds(TurnMoveDuration*2);
+        PlayerTurnText.DOMove(TurnScreenReferencePoints[2].position, TurnMoveDuration).OnComplete(() =>
+        {
+            PlayerTurnText.gameObject.SetActive(true);
+        });
+    }
+
+    private void ConfigPlayerOneTurn()
+    {
+        PlayerTurnText.GetComponent<TextMeshProUGUI>().color = PlayerOneColor;
+        PlayerTurnText.GetComponent<TextMeshProUGUI>().text = "Turno del Jugador 1";
+    }
+
+    private void ConfigPlayerTwoTurn()
+    {
+        PlayerTurnText.GetComponent<TextMeshProUGUI>().color = PlayerTwoColor;
+        PlayerTurnText.GetComponent<TextMeshProUGUI>().text = "Turno del Jugador 2";
+    }
+}
--- Assets/Scripts/Token.cs
+++ Assets/Scripts/Token.cs
@@ -1,5 +1,29 @@
-﻿public class Token
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Token : MonoBehaviour
 {
     public bool isPlayerOne;
     public Cell cell;
+
+    public void MoveToPosition(Vector3 position, float duration)
+    {
+        transform.DOMove(position, duration);
+    }
+
+    public void LookAt(Vector3 target, float duration)
+    {
+        transform.DOLookAt(target, duration);
+    }
+
+    public void JumpToPosition(Vector3 position, float duration)
+    {
+        transform.DOJump(position,1f,1, duration);
+    }
+
+    public void CrushYAxis(float finalY,float duration)
+    {
+        //Los tokens tienen un padre para que solo se escalen en una sola dirección
+        transform.parent.transform.DOScaleY(finalY, duration);
+    }
 }
